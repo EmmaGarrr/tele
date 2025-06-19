e0aff616acc4fc9d0d8a4151d3754b139f77ab77
Nirav Ramani <niravramani@Niravs-Mac-mini.local>
2025-06-19 18:00:51 +0530

    backend url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
     production: true, // <-- Should be true for production builds
-    apiUrl: 'https://13.219.153.84' // <-- CHANGE THIS to your actual production API URL
+    apiUrl: 'https://nirav-tele.onrender.com' // <-- CHANGE THIS to your actual production API URL
   };
